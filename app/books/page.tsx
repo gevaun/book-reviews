e9bd2d5ed8650ed43170add32e269e49554c10c9
Gevaun <geniepinguin@gmail.com>
2024-10-18 22:52:07 -0500

    Remove unused statements

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,28 +1,23 @@
-import client from '@/app/lib/wix'
+import client from "@/app/lib/wix";
 
 export default async function Books() {
-    
-    
-    const books = await client.items.queryDataItems({
-        dataCollectionId: 'Books',
+  const books = await client.items
+    .queryDataItems({
+      dataCollectionId: "Books",
     })
     .find()
-    .then((res) => res.items.map(book => book.data) )
-    
-    
-    console.log(books)
+    .then((res) => res.items.map((book) => book.data));
 
-    return (
-        <div>
-            <h1 className='text-xl font-medium mb-4'>Books</h1>
-            <div className='grid grid-cols-4'>
-                {books.map(book => (
-                    <p key={book?.title}>
-                        {book?.title} by {book?.author}
-                    </p>
-                ))}
-            
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-xl font-medium mb-4">Books</h1>
+      <div className="grid grid-cols-4">
+        {books.map((book) => (
+          <p key={book?.title}>
+            {book?.title} by {book?.author}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
