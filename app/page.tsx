c01b69f7debe1fed55504a81af4934a34b28f520
Gevaun <geniepinguin@gmail.com>
2024-10-21 11:19:38 -0500

    Refactor book page layout and add responsive spacing to book page UI

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className="space-y-4">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <div className="md:grid grid-cols-2 gap-4">
+        <div className="md:grid md:grid-cols-2 gap-4 sm:space-y-4">
           <Image
             className="justify-self-center"
             src="/undraw_book_lover_re_rwjy.svg"
@@ -26,7 +26,7 @@ export default function Home() {
             height={380}
             priority
           />
-          <div className="fxdrfrd  self-center text-center justify-center rounded-2xl py-4 px-10 bg-zinc-50 dark:bg-zinc-900">
+          <div className="self-center text-center justify-center rounded-2xl py-4 px-10 bg-zinc-50 dark:bg-zinc-900">
             <h1 className="text-4xl font-bold mb-2">
               <span className={pacifico.className}>The Books Reviews</span>
             </h1>
@@ -43,6 +43,7 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </main>
         <div className="flex justify-center p-10">
           <Link
             href="/books"
@@ -53,7 +54,6 @@ export default function Home() {
             <ArrowRightCircleIcon className="w-4 inline group-hover:translate-x-1 transition-all duration-200 delay-75" />
           </Link>
         </div>
-      </main>
     </div>
   );
 }
