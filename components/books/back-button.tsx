0f276e1a22820404fedcce168b79ec14a4bd7000
Gevaun <geniepinguin@gmail.com>
2024-10-23 12:50:12 -0500

    Add BackButton component for navigating back in the book page UI

---
+++ components/books/back-button.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
+
+export default function BackButton() {
+  const router = useRouter();
+  return (
+    <div className="mt-4 mb-4" >
+        <span className="gap-2 flex items-center group cursor-pointer" onClick={() => router.back()}>
+      <ArrowLeftIcon className="w-4 h-4 group-hover:-translate-x-1 transition-all duration-200 delay-75" />
+      <span>Back</span>
+
+        </span>
+    </div>
+  );
+}
